35b609b31490029567be38fcdecbae37a11d38cd
Mats Wikström <wikstrom.mats@gmail.com>
2020-01-04 12:16:48 +0100

    Docker from Ormas to wisum

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t ormas/multi-client:latest -t ormas/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t ormas/multi-server:latest -t ormas/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t ormas/multi-worker:latest -t ormas/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t wisum/multi-client:latest -t wisum/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t wisum/multi-server:latest -t wisum/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t wisum/multi-worker:latest -t wisum/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push ormas/multi-client:latest
-docker push ormas/multi-server:latest
-docker push ormas/multi-worker:latest
+docker push wisum/multi-client:latest
+docker push wisum/multi-server:latest
+docker push wisum/multi-worker:latest
 
-docker push ormas/multi-client:$SHA
-docker push ormas/multi-server:$SHA
-docker push ormas/multi-worker:$SHA
+docker push wisum/multi-client:$SHA
+docker push wisum/multi-server:$SHA
+docker push wisum/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=ormas/multi-server:$SHA
-kubectl set image deployments/client-deployment client=ormas/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=ormas/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=wisum/multi-server:$SHA
+kubectl set image deployments/client-deployment client=wisum/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=wisum/multi-worker:$SHA
